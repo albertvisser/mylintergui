552c02ce5b26e7549e6ac07a9f3396b55150d408
Albert Visser <albert.visser@gmail.com>
2017-08-10 16:19:31 +0200

    removed unnecessary comments

--- app/linter_gui.py
+++ app/linter_gui.py
@@ -2,11 +2,6 @@
 """imports the gui from a toolkit-specific module
 so that the starters need not know about which one is used
 """
-## from .linter_ppgui import MainFrame
-## from .linter_tkgui import MainFrame
-## from .linter_tk3gui import MainFrame
-## from .linter_wxgui import MainFrame
-## from .linter_qt4gui import MainFrame
 from .linter_qtgui import MainFrame
 MainFrame = MainFrame
 
