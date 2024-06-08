4f7cb8fc58df329950f57962e453f9530884b6c3
Albert Visser <albert.visser@gmail.com>
2024-06-08 19:22:38 +0200

    finished unittests; restructured subdirs method

--- app/exec.py
+++ app/exec.py
@@ -3,6 +3,7 @@
 de uitvoering wordt gestuurd door in een dictionary verzamelde parameters
 """
 import os
+import pathlib
 import subprocess
 from .config import cmddict, checktypes
 
@@ -53,7 +54,7 @@ class Linter:
                 else:
                     specs.append(" in opgegeven bestanden/directories")
                 for entry in self.p['filelist']:
-                    self.subdirs(entry, is_list=False)
+                    self.subdirs(entry)
             if self.p['subdirs']:
                 specs.append(" en onderliggende directories")
         self.rpt.insert(0, "".join(specs))
@@ -64,74 +65,71 @@ class Linter:
         """get files from repo manifest
         also apply blacklisted names
         """
-        # if not self.p['filelist']:
-        #     # get files from manifest (in case of standalone version of backend)
-        #     # let's assume we'll always be going by the frontend for now
-        #     pass
         self.filenames = []
         for entry in self.p['filelist']:
-            # hlp = pathlib.Path(entry)
-            # if hlp.name in self.p['blacklist']['exclude_files']:
-            if os.path.basename(entry) in self.p['blacklist']['exclude_files']:
+            hlp = pathlib.Path(entry)
+            if hlp.name in self.p['blacklist']['exclude_files']:
+            # if os.path.basename(entry) in self.p['blacklist']['exclude_files']:
                 continue
-            # test = hlp.suffix.lstrip('.')
-            test = os.path.splitext(entry)[1].lstrip('.')
+            test = hlp.suffix[1:]  # .lstrip('.')
+            # test = os.path.splitext(entry)[1].lstrip('.')
             if test not in self.p['blacklist']['include_exts']:
                 continue
             self.filenames.append(entry)
 
-    def subdirs(self, pad, is_list=True, level=0):
-        """recursieve routine voor zoek/vervang in subdirectories
-        samenstellen lijst met te verwerken bestanden
-
-        als is_list = False dan wordt van de doorgegeven naam eerst een list
-        gemaakt. Daardoor hebben we altijd een iterable met directorynamen.
+    def subdirs(self, pad, level=0):
+        """ samenstellen lijsten met te verwerken directories en files
         """
-        if os.path.isdir(pad) and os.path.basename(pad) not in self.p['blacklist']['exclude_dirs']:
-            self.dirnames.add(pad)
         if self.p["maxdepth"] != -1:
             level += 1
             if level > self.p["maxdepth"]:
+                self.rpt.append(f'{pad}: below maximum scanlevel')
                 return
-        if is_list:
+        path = pathlib.Path(pad)
+        if path.is_symlink() and not self.p['follow_symlinks']:
+            return
+        if path.is_dir() and not self.dir_is_blacklisted(path):
+            self.dirnames.add(pad)
             try:
-                _list = [fname.path for fname in os.scandir(pad)]
-            except PermissionError:
-                _list = []
-        else:
-            _list = [pad]
-        for entry in _list:
-            if os.path.isdir(entry):
-                if os.path.basename(entry) in self.p['blacklist']['exclude_dirs']:
-                    continue
-                if self.p['subdirs']:
-                    self.subdirs(entry, level=level)
-            elif os.path.islink(entry) and not self.p['follow_symlinks']:
-                pass
-            else:
-                if os.path.basename(entry) in self.p['blacklist']['exclude_files']:
-                    continue
-                ext = os.path.splitext(entry)[1]
-                if ext and ext[1:] not in self.p['blacklist']['include_exts']:
-                    continue
-                if ext and ext[1:] in self.p['blacklist']['exclude_exts']:
-                    continue
-                if not ext and self.p['blacklist']['include_shebang']:
-                    lines = []
-                    with open(entry) as _in:
-                        lines.append(_in.readline())
-                        lines.append(_in.readline())
-                    skip = True
-                    if any([x[:2] == '#!' for x in lines]):
-                        for line in lines:
-                            if line.startswith('#!'):
-                                for shb in self.p['blacklist']['include_shebang']:
-                                    if shb in line:
-                                        skip = False
-                                        break
-                    if skip:
-                        continue
-                self.filenames.append(entry)
+                _list = list(os.scandir(pad))
+            except PermissionError as err:
+                self.rpt.append(f'could not scan {pad}: {err}')
+                return
+            for entry in _list:
+                self.subdirs(entry.path, level=level)
+        elif path.is_file():
+            if not self.file_is_blacklisted(path):
+                self.filenames.append(pad)
+                try:
+                    path.read_text()
+                    # self.filenames.append(pad)
+                except PermissionError as err:
+                    self.rpt.append(f'could not read {pad}: {err}')
+
+    def file_is_blacklisted(self, entry):
+        "lookup filename in blacklist and return if it should be skipped"
+        if entry.name in self.p['blacklist']['exclude_files']:
+            return True
+        ext = entry.suffix
+        if ext:
+            if (self.p['blacklist']['include_exts']
+                    and ext[1:] not in self.p['blacklist']['include_exts']):
+                return True
+            if ext[1:] in self.p['blacklist']['exclude_exts']:
+                return True
+        elif self.p['blacklist']['include_shebang']:
+            data = entry.read_text().split('\n', 1)[0]  # get first line of file
+            if data.startswith('#!'):
+                for runner in self.p['blacklist']['include_shebang']:
+                    if runner in data:
+                        return True
+        return False
+
+    def dir_is_blacklisted(self, entry):
+        "lookup directory name in blacklist and return if it should be skipped"
+        if entry.name in self.p['blacklist']['exclude_dirs']:
+            return True
+        return False
 
     def do_action(self):
         """do the linting
--- unittests/test_exec.py
+++ unittests/test_exec.py
@@ -85,7 +85,7 @@ class TestLinter:
         assert testobj.results == {}
         assert testobj.specs == ["Gecontroleerd met 'z'", " in xxx"]
         assert capsys.readouterr().out == (
-                "called Linter.subdirs with args ('xxx',) {'is_list': False}\n")
+                "called Linter.subdirs with args ('xxx',) {}\n")
 
         testobj = testee.Linter(filelist=['xxx', 'yyy'], linter='z')
         assert testobj.ok
@@ -95,8 +95,8 @@ class TestLinter:
         assert testobj.results == {}
         assert testobj.specs == ["Gecontroleerd met 'z'", " in opgegeven bestanden/directories"]
         assert capsys.readouterr().out == (
-                "called Linter.subdirs with args ('xxx',) {'is_list': False}\n"
-                "called Linter.subdirs with args ('yyy',) {'is_list': False}\n")
+                "called Linter.subdirs with args ('xxx',) {}\n"
+                "called Linter.subdirs with args ('yyy',) {}\n")
 
         testobj = testee.Linter(filelist=['xxx', 'yyy'], linter='z', subdirs=True)
         assert testobj.ok
@@ -108,8 +108,8 @@ class TestLinter:
         assert testobj.specs == ["Gecontroleerd met 'z'", " in opgegeven bestanden/directories",
                                  " en onderliggende directories"]
         assert capsys.readouterr().out == (
-                "called Linter.subdirs with args ('xxx',) {'is_list': False}\n"
-                "called Linter.subdirs with args ('yyy',) {'is_list': False}\n")
+                "called Linter.subdirs with args ('xxx',) {}\n"
+                "called Linter.subdirs with args ('yyy',) {}\n")
 
     def test_get_from_repo(self, monkeypatch, capsys):
         """unittest for Linter.get_from_repo
@@ -134,12 +134,222 @@ class TestLinter:
         testobj.get_from_repo()
         assert testobj.filenames == ['not-this.py', 'only-this.py']
 
-    def _test_subdirs(self, monkeypatch, capsys):
-        """unittest for Linter.subdirs
+    def test_dir_is_blacklisted(self, monkeypatch, capsys):
+        """unittest for Linter.dir_is_blacklisted
         """
         testobj = self.setup_testobj(monkeypatch, capsys)
-        assert testobj.subdirs('pad', is_list=True, level=0) == "expected_result"
-        assert capsys.readouterr().out == ("")
+        testobj.p["blacklist"] = {"exclude_dirs": ["xxx", "yyy"]}
+        assert not testobj.dir_is_blacklisted(testee.pathlib.Path('zzz'))
+        assert testobj.dir_is_blacklisted(testee.pathlib.Path('xxx'))
+        assert testobj.dir_is_blacklisted(testee.pathlib.Path('here/xxx'))
+
+    def test_file_is_blacklisted(self, monkeypatch, capsys, tmp_path):
+        """unittest for Linter.file_is_blacklisted
+        """
+        testobj = self.setup_testobj(monkeypatch, capsys)
+        testobj.p["blacklist"] = {"exclude_files": ['xxx', '.yyy']}
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('xxx'))
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('.yyy'))
+        testobj.p["blacklist"] = {"exclude_files": [],
+                                  "include_exts": [],
+                                  "exclude_exts": ['b', 'c'],
+                                  "include_shebang": []}
+        assert not testobj.file_is_blacklisted(testee.pathlib.Path('xxx.a'))
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('zzz.b'))
+        path = testee.pathlib.Path('zzz')
+        assert not testobj.file_is_blacklisted(path)
+        testobj.p["blacklist"] = {"exclude_files": [],
+                                  "include_exts": ['a', 'b'],
+                                  "exclude_exts": ['b', 'c'],
+                                  "include_shebang": ['bin', 'rrr']}
+        assert not testobj.file_is_blacklisted(testee.pathlib.Path('xxx.a'))
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('zzz.b'))
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('xxx.c'))
+        assert testobj.file_is_blacklisted(testee.pathlib.Path('zzz.d'))
+        testfile = tmp_path / 'zzz'
+        testfile.touch()
+        assert not testobj.file_is_blacklisted(testfile)
+        testfile.write_text('#! bin\ndo_something')
+        assert testobj.file_is_blacklisted(testfile)
+        testfile.write_text('#! /qqq/rrr')
+        assert testobj.file_is_blacklisted(testfile)
+        testfile.write_text('#! qqq\n')
+        assert not testobj.file_is_blacklisted(testfile)
+
+    def test_subdirs(self, monkeypatch, capsys):
+        """unittest for Linter.subdirs
+        """
+        class MockDirEntry:
+            def __init__(self, name):
+                self.path = name
+        def mock_isfile(*args):
+            print('called path.is_file')
+            return True
+        def mock_is_not_file(*args):
+            print('called path.is_file')
+            return False
+        counter_d = 0
+        def mock_is_dir(*args):
+            nonlocal counter_d
+            print('called path.is_dir')
+            counter_d += 1
+            return counter_d == 1
+        def mock_is_not_dir(*args):
+            print('called path.is_dir')
+            return False
+        def mock_issymlink(*args):
+            print('called path.is_symlink')
+            return True
+        def mock_is_not_symlink(*args):
+            print('called path.is_symlink')
+            return False
+        def mock_scandir(pad):
+            print(f"called os.scandir with arg '{pad}'")
+            return [MockDirEntry('xxx')]
+        def mock_scandir_err(pad):
+            print(f"called os.scandir with arg '{pad}'")
+            raise PermissionError("access denied")
+        def mock_dir_blacklisted(entry):
+            print(f"called Linter.dir_is_blacklisted with arg '{entry}'")
+            return True
+        def mock_dir_not_blacklisted(entry):
+            print(f"called Linter.dir_is_blacklisted with arg '{entry}'")
+            return False
+        counter_d_b = 0
+        def mock_dir_not_blacklisted_2(entry):
+            nonlocal counter_d_b
+            print(f"called Linter.dir_is_blacklisted with arg '{entry}'")
+            counter_d_b += 1
+            return not counter_d_b == 1
+        def mock_file_blacklisted(entry):
+            print(f"called Linter.file_is_blacklisted with arg '{entry}'")
+            return True
+        def mock_file_not_blacklisted(entry):
+            print(f"called Linter.file_is_blacklisted with arg '{entry}'")
+            return False
+        def mock_read(self):
+            "stub"
+        def mock_read_err(self):
+            raise PermissionError("access denied")
+
+        testobj = self.setup_testobj(monkeypatch, capsys)
+        testobj.p['maxdepth'] = 1
+        testobj.p['follow_symlinks'] = False
+        testobj.p['subdirs'] = True
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.dir_is_blacklisted = mock_dir_blacklisted
+        testobj.file_is_blacklisted = mock_file_blacklisted
+        monkeypatch.setattr(testee.pathlib.Path, 'is_symlink', mock_issymlink)
+        monkeypatch.setattr(testee.pathlib.Path, 'is_dir', mock_is_not_dir)
+        monkeypatch.setattr(testee.pathlib.Path, 'is_file', mock_isfile)
+        monkeypatch.setattr(testee.os, 'scandir', mock_scandir_err)
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('mypath', level=1)
+        assert testobj.dirnames == set()
+        assert testobj.filenames == []
+        assert testobj.rpt == ['mypath: below maximum scanlevel']
+        assert capsys.readouterr().out == ""
+
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('mypath')
+        assert testobj.dirnames == set()
+        assert testobj.filenames == []
+        assert testobj.rpt == []
+        assert capsys.readouterr().out == "called path.is_symlink\n"
+
+        testobj.p['follow_symlinks'] = True
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('myfile')
+        assert testobj.dirnames == set()
+        assert testobj.filenames == []
+        assert testobj.rpt == []
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called path.is_file\n"
+                                           "called Linter.file_is_blacklisted with arg 'myfile'\n")
+
+        testobj.p['maxdepth'] = -1
+        monkeypatch.setattr(testee.pathlib.Path, 'is_symlink', mock_is_not_symlink)
+        monkeypatch.setattr(testee.pathlib.Path, 'read_text', mock_read)
+        testobj.file_is_blacklisted = mock_file_not_blacklisted
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('myfile')
+        assert testobj.dirnames == set()
+        assert testobj.filenames == ['myfile']
+        assert testobj.rpt == []
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called path.is_file\n"
+                                           "called Linter.file_is_blacklisted with arg 'myfile'\n")
+
+        monkeypatch.setattr(testee.pathlib.Path, 'read_text', mock_read_err)
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('myfile')
+        assert testobj.dirnames == set()
+        assert testobj.filenames == ['myfile']
+        assert testobj.rpt == ['could not read myfile: access denied']
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called path.is_file\n"
+                                           "called Linter.file_is_blacklisted with arg 'myfile'\n")
+
+        monkeypatch.setattr(testee.pathlib.Path, 'is_dir', mock_is_dir)
+        monkeypatch.setattr(testee.pathlib.Path, 'is_file', mock_is_not_file)
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('mydir')
+        assert testobj.dirnames == set()
+        assert testobj.filenames == []
+        assert testobj.rpt == []
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called Linter.dir_is_blacklisted with arg 'mydir'\n"
+                                           "called path.is_file\n")
+
+        counter_d = 0
+        testobj.dir_is_blacklisted = mock_dir_not_blacklisted
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('mydir')
+        assert testobj.dirnames == {'mydir'}
+        assert testobj.filenames == []
+        assert testobj.rpt == ['could not scan mydir: access denied']
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called Linter.dir_is_blacklisted with arg 'mydir'\n"
+                                           "called os.scandir with arg 'mydir'\n")
+
+        counter_d = 0
+        monkeypatch.setattr(testee.os, 'scandir', mock_scandir)
+        testobj.dir_is_blacklisted = mock_dir_not_blacklisted_2
+        testobj.dirnames = set()
+        testobj.filenames = []
+        testobj.rpt = []
+        testobj.subdirs('mydir')
+        assert testobj.dirnames == {'mydir'}
+        assert testobj.filenames == []
+        assert testobj.rpt == []
+        assert capsys.readouterr().out == ("called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called Linter.dir_is_blacklisted with arg 'mydir'\n"
+                                           "called os.scandir with arg 'mydir'\n"
+                                           "called path.is_symlink\n"
+                                           "called path.is_dir\n"
+                                           "called path.is_file\n")
 
     def test_do_action(self, monkeypatch, capsys):
         """unittest for Linter.do_action
