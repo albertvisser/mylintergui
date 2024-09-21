b86b46e914fe4cc9806d2f8edd0c1380215c6218
Albert Visser <albert.visser@gmail.com>
2024-09-21 11:43:04 +0200

    removed old code

--- app/linter_gui.py
+++
@@ -1,5 +0,0 @@
-"""imports the gui from a toolkit-specific module
-so that the starters need not know about which one is used
-for switching out gui toolkits, use a conditional import (see various other of my projects)
-"""
-from .linter_qtgui import MainFrame
